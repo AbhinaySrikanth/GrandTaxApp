b56d8fecebac1be655d5fc357cc4f68162c034c0
AbhinaySrikanth <134436280+AbhinaySrikanth@users.noreply.github.com>
2025-05-20 20:38:31 +0530

    Initial commit

---
+++ app/src/main/java/com/GrandTaxFilers/grandtaxapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.GrandTaxFilers.grandtaxapp
+
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsetsController
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview)
+        webView.webViewClient = WebViewClient()
+
+
+
+        val settings: WebSettings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.loadWithOverviewMode = true
+        settings.useWideViewPort = true
+
+        webView.loadUrl("https://www.grandtaxfilers.com/")
+    }
+
+    public override fun onDestroy() {
+        super.onDestroy()
+        webView.destroy()
+    }
+}
---
+++ app/src/main/java/com/GrandTaxFilers/grandtaxapp/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.GrandTaxFilers.grandtaxapp
+import android.content.Intent
+import android.os.Bundle
+import android.os.Looper
+import androidx.activity.ComponentActivity
+
+class SplashActivity : ComponentActivity() {
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.splash)
+    android.os.Handler(Looper.getMainLooper()).postDelayed({
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }, 2000)
+}
+}
